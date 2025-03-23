be46c97f19910dca86e72986615e2922cfad1dbb
carlosrs14 <crincones578@gmail.com>
2025-03-23 15:06:38 -0500

    adding lastnumber functioanily

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
-let secretNumber = parseInt(Math.random() * 100);
+let secretNumber = parseInt(Math.random() * 100) + 1;
 const form = document.getElementById("form");
-const number = document.getElementById("number");
+const numberInput = document.getElementById("number");
+const contador = document.getElementById("contador");
+const listaIntentos = document.getElementById("lista-intentos");
+
+var intentos = 0;
+var numerosIngresados = new Set();
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     
-    validateWinner();
+    let userNumber = parseInt(numberInput.value);
+    
+    if (numerosIngresados.has(userNumber)) {
+        alert("ya has ingresado este numero");
+        return;
+    }
+    
+    numerosIngresados.add(userNumber);
+    intentos++;
+    contador.textContent = intentos;
+
+    let li = document.createElement("li");
+    li.className = "intento";
+    li.textContent = userNumber;
+
+    listaIntentos.prepend(li);
+
+    validateWinner(userNumber);
 });
 
-function validateWinner() {
-    if (secretNumber < number.value) {
+function validateWinner(userNumber) {
+    if (secretNumber < userNumber) {
         alert("tu numero es mayor");
         return;
     }
-    if (secretNumber > number.value) {
+    if (secretNumber > userNumber) {
         alert("tu numero es menor");
         return;
     }
